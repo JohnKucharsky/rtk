848f0f269f067e4f7563e61d118e5251b5ac4c71
John <vannok@live.com>
2022-07-05 01:05:14 +0300

    cmmt

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,9 +1,3 @@
-export interface Todo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
 export type Contact = {
   userId: number;
   id: number;
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { Contact, Todo } from "../models/todo.model";
+import { Contact } from "../models/todo.model";
 
 export const todosApi = createApi({
   reducerPath: "todosApi",
